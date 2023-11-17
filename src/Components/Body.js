b1cca5ded9b9518f344d3b6c56de9086210487b5
MohamedJunaid7 <junaidtousif7896@gmail.com>
2023-11-17 11:06:40 +0530

    one

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -16,7 +16,7 @@ this.setState({count: this.state.count-1})
 render(){
   return(
     <div>
-          <ReactPlayer url={'https://www.youtube.com/watch?v=XfXgUZDCAoA'} controle height={500} width={1500} />
+          <ReactPlayer url={'https://www.youtube.com/watch?v=XfXgUZDCAoA'} controle height={500} width={500} />
                     <p> An image is a visual representation of something. An image can be a two-dimensional (2D) representation, such as a drawing, painting, or photograph, or a three-dimensional (3D) object, such as a carving or sculpture.</p>
 
       <img src='https://th.bing.com/th/id/OIP.JSsToB-lGbPi1xiDUmK03wHaEo?w=282&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7' alt="Description of the image" className='image' />
